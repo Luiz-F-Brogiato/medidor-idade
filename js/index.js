5626ab8f90961cf90af68bb19429fcb24c2d9615
Luiz Fernando <brogiatoluizfernando@gmail.com>
2025-06-09 20:55:19 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 function verificar() {
     var data = new Date();
     var ano = data.getFullYear();
@@ -6,45 +7,39 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         alert(' [ERRO]Verifique os dados e tente novamente');
     } else {
-        mudaImagem()
-    }
-}
+        var fsex = document.querySelectorAll('input[name="radsex"]');
+        var idade = fano.value;
+        console.log(idade)
+        var genero = '';
+        var img = document.createElement('img');
+        img.setAttribute('id', 'foto');
+        if (fsex[0].checked == true) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'bebe-masculino.png');
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem-masculino.png');
+            } else if (idade < 50) {
+                img.setAttribute('src', 'adulto-masculino.png');
+            } else {
+                img.setAttribute('src', 'idoso-masculino.png');
+            }
 
-function mudaImagem() {
-    let data = new Date();
-    let ano = data.getFullYear();
-    let fano = document.getElementById('txtano');
-    let res = document.querySelector('div#res');
-    let fsex = document.querySelectorAll('input[name="radsex"]');
-    let idade = fano.value;
-    let anoNascimento = ano - idade;
-    let img = document.createElement('img');
-    switch (fsex[0].checked == true) {
-        case idade >= 50:
-            img.setAttribute('src', 'idoso-masculino.png');
-            break;
-        case idade > 21:
-            img.setAttribute('src', 'adulto-masculino.png');
-            break;
-        case idade >= 10 && idade < 21:
-            img.setAttribute('src', 'jovem-masculino.png');
-            break;
-        default:
-        img.setAttribute('src', 'bebe-feminina.png');
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'bebe-feminina.png');
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem-feminina.png');
+            } else if (idade < 50) {
+                img.setAttribute('src', 'adulto-feminina.png');
+            } else {
+                img.setAttribute('src', 'idoso-feminino.png');
+            }
+        }
+        res.innerHTML = `Nascido(a) em ${idade}.`
+        res.appendChild(img);
+       
     }
-    switch (fsex[1].checked == true) {
-        case idade >= 50:
-            img.setAttribute('src', 'idoso-feminino.png');
-            break;
-        case idade > 21:
-            img.setAttribute('src', 'adulto-feminina.png');
-            break;
-        case idade >= 10 && idade < 21:
-            img.setAttribute('src', 'jovem-feminina.png');
-            break;
-        default:
-        img.setAttribute('src', 'bebe-feminina.png');
-    }
-    res.innerHTML = `Nascido(a) em ${anoNascimento}.`
-    res.appendChild(img);
+
 }
